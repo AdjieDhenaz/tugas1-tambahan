02a45dd6dec80176d2c2f4cfc77dc59ecf9e7cd4
AdjieDhenaz <adjiedhenaz21@gmail.com>
2022-02-03 12:44:28 -0800

    tugas_js

---
+++ soal3.js
@@ -0,0 +1,12 @@
+function comp(huruf1, huruf2) {
+    if (huruf1.length == huruf2.length) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+ }
+ console.log(comp("ab", "cd"));
+ console.log(comp("abc", "de"));
+ console.log(comp("hello", "edabit"));
